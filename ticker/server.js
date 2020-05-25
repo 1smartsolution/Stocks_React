6926835eb9c36828757c507f46beba27915ca738
Eduard <1smartsolution@gmail.com>
2020-05-25 19:57:49 -0400

    .

--- ticker/server.js
+++
@@ -1,9 +0,0 @@
-var webpack = require('webpack')
-var WebpackDevServer = require('webpack-dev-server')
-var config = require('./webpack.config')
-
-new WebpackDevServer(webpack(config), {
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, res) {
-  err ? console.log(err) : console.log("Listening at localhost:3000")
-})
